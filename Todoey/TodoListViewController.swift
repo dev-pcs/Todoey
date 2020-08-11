e9c42d3fe34f53d2db33c985eb09836a7177fcc8
Priyank Shah <priyankshah@MacBook-Pro.attlocal.net>
2020-08-11 14:54:04 -0500

    checkmark functionality added

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -17,7 +17,7 @@ class TodoListViewController: UITableViewController {
     }
     
     
-    //MARK: - TableView Datasource methods
+    //MARK: - TableView Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
@@ -29,6 +29,23 @@ class TodoListViewController: UITableViewController {
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
     }
+    
+    
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        //tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
     
 }
 
